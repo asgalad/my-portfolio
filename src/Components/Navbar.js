a55133220a2cc0527a3ecb3a55028d33e371d7bb
asgalad <abbakarhaneef@gmail.com>
2022-08-18 10:55:29 +0000

    First commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () =>{
+    return (
+        <>
+        <div className='Nav'>
+        <h4>React Learning</h4>
+        <ul className='Unorderd'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Join us</li>
+            <li>Home again</li>
+        </ul>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
