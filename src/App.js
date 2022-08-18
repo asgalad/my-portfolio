8e4f412622ca390e7c1939746e7ecd275d505628
asgalad <abbakarhaneef@gmail.com>
2022-08-18 16:39:34 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import {
-  Container,
-  Box,
-  Heading,
-  Text,
-  Image,
-  Button,
-  Divider,
-  List,
-  ListItem,
-  Icon,
-  Link,
-} from "@chakra-ui/react";
-
-
 function App() {
   return (
     <>
-    <Navbar/>
-    <Box as="main" background={"blue.500"} >
-      <Container maxW="container.lg" background={"red.200"} pt={14}>
-       <Text pb={20} color={"blue"}>How far</Text>
-      </Container>
-    </Box>
    
   
     </>
